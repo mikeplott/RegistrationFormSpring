ea1acd917c159d6de48567c890da62a8b03c2e24
michaelplott <mike.plott80@gmail.com>
2016-10-26 10:43:10 -0400

    Finished connecting the front end and backend

--- src/main/java/com/theironyard/controllers/RegistrationFormSpringController.java
+++ src/main/java/com/theironyard/controllers/RegistrationFormSpringController.java
@@ -1,15 +1,25 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.User;
+import com.theironyard.services.UserRepository;
 import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by michaelplott on 10/26/16.
  */
+@RestController
 public class RegistrationFormSpringController {
+    @Autowired
+    UserRepository users;
+
     Server h2Server;
 
     @PostConstruct
@@ -20,5 +30,25 @@ public class RegistrationFormSpringController {
     @PreDestroy
     public void destroy() {
         h2Server.stop();
+    }
+
+    @RequestMapping(path = "/user", method = RequestMethod.POST)
+    public User addUser(@RequestBody User user) {
+        return users.save(user);
+    }
+
+    @RequestMapping(path = "/user", method = RequestMethod.GET)
+    public Iterable<User> getUsers() {
+        return users.findAll();
+    }
+
+    @RequestMapping(path = "/user", method = RequestMethod.PUT)
+    public User updateUser(@RequestBody User user) {
+        return users.save(user);
+    }
+
+    @RequestMapping(path = "/user/{id}", method = RequestMethod.DELETE)
+    public void deleteUser(@PathVariable("id") int id) {
+        users.delete(id);
     }
 }
